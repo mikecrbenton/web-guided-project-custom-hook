7cd448d9407bf0741a4e29af1e5152883cb45e80
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-02 13:35:13 -0500

    lecture work-in-progress

--- followAlong/src/hooks/useForm.js
+++ followAlong/src/hooks/useForm.js
@@ -4,8 +4,6 @@ const useForm = (formFields)=> {
     const [values, setValues] = useState(formFields);
   
     const handleChanges = e => {
-      console.log(e.target.name);
-      
       setValues({
         ...values,
         [e.target.name]: e.target.value

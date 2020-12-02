bb4aa4c0e8c25ae104591ea54d9e2bcd353370bc
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-02 13:52:51 -0500

    lecture work-in-progress

--- followAlong/src/hooks/useForm.js
+++ followAlong/src/hooks/useForm.js
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 
 const useLocalStorage = (initialValue) => {
+    //1. When we setup state, check to see if our value is already inside of localStorage.
+    //2. If value is not in localStorage, set initialValue to localStorage
+    //3. When we set state value, also set localStorage value.
+    
     const [storedValue, setStoredValue]  = useState();
 }
 
